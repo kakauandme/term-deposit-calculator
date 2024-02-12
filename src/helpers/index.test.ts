ffec1b040bb21508538eb4e837cc53fb1268da04
Kirill Kliavin <k.kliavin@gmail.com>
2024-02-12 23:00:58 +1100

    compoundInterest helper

---
+++ src/helpers/index.test.ts
@@ -0,0 +1,46 @@
+import { describe, it, expect } from 'vitest'
+
+import { compoundInterest } from './index'
+
+describe('compoundInterest', () => {
+  it('Task example monthly', () => {
+    const amount = 10000
+    const rate = 0.011
+    const term = 3
+    const frequency = 12
+
+    const result = compoundInterest(amount, rate, term, frequency)
+
+    expect(result).toBeCloseTo(10335, 0)
+  })
+  it('Task example quarterly', () => {
+    const amount = 10000
+    const rate = 0.011
+    const term = 3
+    const frequency = 4
+
+    const result = compoundInterest(amount, rate, term, frequency)
+
+    expect(result).toBeCloseTo(10335, 0)
+  })
+  it('Task example annualy', () => {
+    const amount = 10000
+    const rate = 0.011
+    const term = 3
+    const frequency = 1
+
+    const result = compoundInterest(amount, rate, term, frequency)
+
+    expect(result).toBeCloseTo(10334, 0)
+  })
+  it('Task example at maturity ', () => {
+    const amount = 10000
+    const rate = 0.011
+    const term = 3
+    const frequency = 1 / term
+
+    const result = compoundInterest(amount, rate, term, frequency)
+
+    expect(result).toBeCloseTo(10330, 0)
+  })
+})
---
+++ src/helpers/index.ts
@@ -0,0 +1,34 @@
+/**
+ * compoundInterest function calculates the compound interest over a given term.
+ *
+ * @param {number} amount - The principal amount, the initial amount of money that is being compounded.
+ * @param {number} rate - The annual interest rate (in decimal form, so 5% would be 0.05).
+ * @param {number} term - The term over which the interest is compounded, in years.
+ * @param {number} frequency - The number of times the interest is compounded per year.
+ *
+ * @returns {number} - The final balance amount after compounding.
+ */
+
+export const compoundInterest = (amount: number, rate: number, term: number, frequency: number) => {
+  if (amount <= 0) {
+    throw new Error('Principal has to be more than $0 ')
+  }
+  if (rate <= 0 || rate > 1) {
+    throw new Error('Rate must be between 0 and 1')
+  }
+  if (term <= 0) {
+    throw new Error('Term must be more than 0 years')
+  }
+  if (frequency <= 0) {
+    throw new Error('Frequency must be more than 0')
+  }
+
+  try {
+    return amount * Math.pow(1 + rate / frequency, frequency * term)
+  } catch (error) {
+    // Handle the error here
+    console.error(error)
+    // Return a default value or rethrow the error
+    throw error
+  }
+}
