02eedeb2c44f1a163c148f74916cf3b83dc87a70
Kirill Kliavin <k.kliavin@gmail.com>
2024-02-12 22:13:38 +1100

    vite+vue+bootstrap

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <main class="container">
+    <HelloWorld msg="You did it!" />
+  </main>
+</template>
+
+<style scoped></style>
